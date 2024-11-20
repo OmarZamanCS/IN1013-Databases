61bf8672d69488bf1ffbdc9e54557aba9db25746
OmarZamanCS <omar.zaman.2@city.ac.uk>
2024-11-20 20:41:38 +0000

    updated task 1 and task 2

--- HW 5/task1.sql
+++ HW 5/task1.sql
@@ -1,3 +1,4 @@
+
 1.
 SELECT bill_date, bill_total
 FROM restBill
@@ -12,15 +13,20 @@ ORDER BY cust_name DESC;
 3.
 SELECT DISTINCT cust_name
 FROM restBill
-WHERE cust_name LIKE '% C%'
-ORDER BY cust_name;
+WHERE cust_name LIKE '% C%';
 
 4.
-SELECT DISTINCT s.first_name, s.surname
-FROM restStaff s
-WHERE s.staff_no IN (SELECT DISTINCT headwaiter FROM)
+SELECT CONCAT(first_name, ' ', surname) AS headwaiter_name
+FROM restStaff
+WHERE headwaiter IS NULL;
 
 5.
+SELECT *
+FROM restBill
+WHERE bill_date BETWEEN 160201 AND 160229;
 
 6.
-
+SELECT DISTINCT bill_date
+FROM restBill
+WHERE bill_date BETWEEN 150101 AND 151231
+ORDER BY bill_date;
